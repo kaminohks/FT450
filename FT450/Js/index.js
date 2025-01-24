b43b304d42286768dd7be9733ba3e71897158af8
Arthur Murray <arthur.dartora.tds2023@gmail.com>
2025-01-24 14:14:49 -0300

    Add files via upload

---
+++ FT450/Js/index.js
@@ -0,0 +1,45 @@
+import Start from "./start.js"
+import Input from "./input.js"
+
+const tela = document.querySelector("canvas");
+const pincel = tela.getContext("2d");
+
+
+//verificação da pagina local
+if(document.getElementById('injecaoCanvas')){
+    const inicio = new Start(pincel,tela);
+    inicio.iniciarCirculos(inicio.canvas.x, inicio.canvas.valor, inicio.valorSelecionado, inicio.itemSelecionado);
+    const inputHandler = new Input(pincel,tela);
+    
+
+}
+if(document.getElementById('compensacaoRotacao')){
+    const inicio = new Start(pincel,tela);
+    inicio.iniciarCirculos(inicio.canvas.x, inicio.canvas.valor, inicio.compensacaoRotacao.valorSelecionado, inicio.compensacaoRotacao.itemSelecionado);
+    const inputHandler = new Input(pincel,tela);
+    
+
+}
+if(document.getElementById('lentaTps')){
+    const inicio = new Start(pincel,tela);
+    console.log(inicio.lentaTps.itemSelecionado);
+    inicio.iniciarCirculos(inicio.canvas.x, inicio.canvas.valor, inicio.lentaTps.valorSelecionado, inicio.lentaTps.itemSelecionado);
+    const inputHandler = new Input(pincel,tela);
+    
+}
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
